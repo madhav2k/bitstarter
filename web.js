80b9fbc4bf0b0bc3a764c266f5dcdd5fcb9b9ba7
Ubuntu <ubuntu@ip-172-31-36-165.us-west-2.compute.internal>
2013-07-10 04:20:49 +0000

    Attempt 2 at reading from index.html

--- web.js
+++ web.js
@@ -3,12 +3,6 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-
-fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-
 var buffer = new Buffer("I'm a string!", "utf-8");
 app.get('/', function(request, response) {
   response.send(buffer.toString());
