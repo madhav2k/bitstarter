a31ac551fa8252eaee7e7f202058f8b10faa1078
Ubuntu <ubuntu@ip-172-31-36-165.us-west-2.compute.internal>
2013-07-10 05:50:38 +0000

    I think I have the final implementation

--- web.js
+++ web.js
@@ -1,22 +1,34 @@
 var express = require('express');
 var fs = require("fs");
-var app = express.createServer(express.logger());
 
-var buffer = new Buffer("I'm a string!", "utf-8");
-var fileName = "index.html";
+var fileName = 'index.html';
+var app = express.createServer(express.logger());
+var dataOut = '';
 fs.exists(fileName, function(exists) {
   if (exists) {
-     buffer = "file found";
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+          //console.log(dataOut);
+          sendWebResponse(data);
+          fs.close(fd);
+        });
+      });
+    });
   }
-  else{
-    buffer = "no file";
-  }
-
-app.get('/', function(request, response) {
-  response.send(buffer.toString());
 });
-
+funtion sendWebResponse(inStream){
+app.get('/', function(request, response) {
+  response.send(inStream); 
+});      
+          
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+});         
+}
+//function logData(inStream){
+//console.log(inStream);
+//}
