6827d2b802d84482fb83df3257127ba04db2e17a
Ubuntu <ubuntu@ip-172-31-36-165.us-west-2.compute.internal>
2013-07-10 05:56:47 +0000

    Corrected an error with function params

--- web.js
+++ web.js
@@ -3,32 +3,34 @@ var fs = require("fs");
 
 var fileName = 'index.html';
 var app = express.createServer(express.logger());
-var dataOut = '';
-fs.exists(fileName, function(exists) {
+
+function readFile(whichFile){
+fs.exists(whichFile, function(exists) {
   if (exists) {
-    fs.stat(fileName, function(error, stats) {
-      fs.open(fileName, "r", function(error, fd) {
+    fs.stat(whichFile, function(error, stats) {
+      fs.open(whichFile, "r", function(error, fd) {
         var buffer = new Buffer(stats.size);
         fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
           var data = buffer.toString("utf8", 0, buffer.length);
           //console.log(dataOut);
-          sendWebResponse(data);
+          //osendWebResponse(data);
+          return data;
           fs.close(fd);
         });
       });
     });
   }
 });
-funtion sendWebResponse(inStream){
+}
+
 app.get('/', function(request, response) {
-  response.send(inStream); 
+  response.send(readFile(fileName)); 
 });      
           
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });         
-}
 //function logData(inStream){
 //console.log(inStream);
 //}
