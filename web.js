5a4ffefdadc1ddc69b0a06cc0b7ae908bb66829b
Ubuntu <ubuntu@ip-172-31-36-165.us-west-2.compute.internal>
2013-07-10 06:07:52 +0000

    more changes

--- web.js
+++ web.js
@@ -1,36 +1,43 @@
-var express = require('express');
+//var express = require('express');
 var fs = require("fs");
 
 var fileName = 'index.html';
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
-function readFile(whichFile){
-fs.exists(whichFile, function(exists) {
+fs.exists(fileName, function(exists) {
   if (exists) {
-    fs.stat(whichFile, function(error, stats) {
-      fs.open(whichFile, "r", function(error, fd) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
         var buffer = new Buffer(stats.size);
         fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
           var data = buffer.toString("utf8", 0, buffer.length);
           //console.log(dataOut);
           //osendWebResponse(data);
-          return data;
+app.get('/', function(request, response) {
+  response.send(data); 
+});      
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});         
+          //return data;
           fs.close(fd);
         });
       });
     });
   }
 });
-}
 
-app.get('/', function(request, response) {
-  response.send(readFile(fileName)); 
-});      
+//app.get('/', function(request, response) {
+//  response.send(readFile(fileName)); 
+//});      
           
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});         
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});         
 //function logData(inStream){
 //console.log(inStream);
 //}
+console.log(readFile(fileName));
