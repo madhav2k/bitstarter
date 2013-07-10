d5e6aaf5a1844dc1be0bd1d22ac136986e39f66a
Ubuntu <ubuntu@ip-172-31-36-165.us-west-2.compute.internal>
2013-07-10 04:16:26 +0000

    Reading from index.html into web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+var buffer = new Buffer("I'm a string!", "utf-8");
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
