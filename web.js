7cd72fdfa6a9504e56c0c41b36b4e864da6f527f
Ubuntu <ubuntu@ip-172-31-36-165.us-west-2.compute.internal>
2013-07-22 06:03:15 +0000

    HW4 Index.html

--- web.js
+++ web.js
@@ -23,7 +23,7 @@ fs.exists(fileName, function(exists) {
             response.send(data); 
           });      
           
-          var port = process.env.PORT || 5000;
+          var port = process.env.PORT || 8080;
           app.listen(port, function() {
           console.log("Listening on " + port);
           });         
